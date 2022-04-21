7e9c6d787c859746e48cd76cd4ddd39f3f9d1574
olga-kuriatnyk <olhak@uw.edu>
2022-04-21 12:13:44 -0700

    fib number func

---
+++ DynamicProgramming/FibMemoization.java
@@ -0,0 +1,55 @@
+// a function fib(n) takes in a number as an argument and
+// returns the n-th number of the Fibonacci sequence
+
+package DynamicProgramming;
+
+import java.util.HashMap;
+
+public class FibMemoization {
+
+    private static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public static void main(String[] arg) {
+
+        // recursive solition tests
+        System.out.println(fibRecursive(6)); // 8
+        System.out.println(fibRecursive(7)); // 13
+        System.out.println(fibRecursive(8)); // 21
+
+        // dynamic solution, memoization tests
+        memo.put(0, 0);
+        memo.put(1, 1);
+        System.out.println(fib(6)); // 8
+
+        memo.clear();
+        memo.put(0, 0);
+        memo.put(1, 1);
+        System.out.println(fib(8)); // 21
+
+        memo.clear();
+        memo.put(0, 0);
+        memo.put(1, 1);
+        System.out.println(fib(48)); // 512559680
+    }
+
+    // dynamic solution, memoization
+    // key will be arg to function, value will be the return value
+    // time and space complexity O(n)
+    private static int fib(int n) {
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        memo.put(n, fib(n - 1) + fib(n - 2));
+        return memo.get(n);
+    }
+
+    // recursive solution, doesn't work on big numbers like fib(50)
+    // complexity is O(2^n)
+    private static int fibRecursive(int n) {
+        if (n <= 2)
+            return 1;
+        return fibRecursive(n - 1) + fibRecursive(n - 2);
+    }
+}
