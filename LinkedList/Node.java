3c80eaf911fc0e5979771e9ba6b3f2cc552d37f1
olga-kuriatnyk <olhak@uw.edu>
2022-05-09 15:43:54 -0700

    add linked list traversal

---
+++ LinkedList/Node.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Node {
+    char val;
+    Node next;
+    Node() {}
+    Node(char val) { this.val = val; this.next = null; }
+    Node(char val, Node next) { this.val = val; this.next = next; }
+}
+
+
---
+++ LinkedList/Traversal.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class Traversal {
+    
+    public static void main(String[] arg) {
+        Node a = new Node('A');
+        Node b = new Node('B');
+        Node c = new Node('C');
+        Node d = new Node('D');
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        // printLinkedList(a);
+        printLinkedListRecursive(a);
+
+        Node e = new Node('E');
+        d.next = e;
+        System.out.println(linkedListValues(a));
+    }
+
+    public static void printLinkedList(Node head) {
+        Node curr = head;
+        while(curr != null) {
+            System.out.println(curr.val);
+            curr = curr.next;
+        }
+    } 
+
+    public static void printLinkedListRecursive(Node head) {
+        if (head == null) return;
+        System.out.println(head.val);
+        printLinkedListRecursive(head.next);
+    } 
+    
+    public static  ArrayList<Character>linkedListValues(Node head) {
+        Node curr = head;
+        ArrayList<Character> list = new ArrayList<Character>();
+        while (curr != null) {
+            list.add(curr.val);
+            curr = curr.next;
+        }
+        return list;
+    }
+
+}
+
